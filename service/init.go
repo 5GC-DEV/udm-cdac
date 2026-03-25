48cb86deb88e5af5284877436b9b0d59f11f133a
bhagathkrishnacdac <bhagath.krishna@cdac.in>
2026-03-25 16:31:15 +0530

    Fix: Lint issue

--- service/init.go
+++ service/init.go
@@ -142,8 +142,12 @@ func checkClientHealth(client grpcClient.ConfClient, count *int) bool {
 }
 
 // ensureConfigSubscription ensures the GRPC stream and config channel are initialized.
-func ensureConfigSubscription(client grpcClient.ConfClient, stream *protos.ConfigService_NetworkSliceSubscribeClient,
-	configChannel *chan *protos.NetworkSliceResponse, udm *UDM) {
+func ensureConfigSubscription(
+	client grpcClient.ConfClient,
+	stream *protos.ConfigService_NetworkSliceSubscribeClient,
+	configChannel *chan *protos.NetworkSliceResponse,
+	udm *UDM,
+) {
 	var err error
 	// 1. Ensure Stream
 	if *stream == nil {
